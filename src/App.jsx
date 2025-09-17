def5ea34150e14f654ead477f2555f318785a01a
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-09-17 16:13:41 +1000

    Add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import viteLogo from '/vite.svg';
-import { useState } from 'react';
-
-import './App.css';
-import reactLogo from './assets/react.svg';
+import './App.scss';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href='https://vite.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
+		<div className='wrapper'>
+			<header>
+				<div className='headerLeft'>
+					<svg />
+					<div className='headerInfo'>
+						<h3>React Sneakers</h3>
+						<p>Магазин лучших кроссовок</p>
+					</div>
+				</div>
+				<ul className='headerRight'>
+					<li>
+						<svg />
+						<span>1205 руб.</span>
+					</li>
+					<li>
+						<svg />
+					</li>
+				</ul>
+			</header>
+			<div className='content'>
+				<h1>Все кроссовки</h1>
 			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</div>
 	);
 }
 
