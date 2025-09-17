4ce375e31d4a173a0ba48ea1ee2e24f2767b30ae
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-09-17 19:18:35 +1000

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
+import Header from '@/components/Header';
+
 import './App.scss';
 
 function App() {
 	return (
-		<div className='wrapper'>
-			<header>
-				<div className='headerLeft'>
-					<img width={40} height={40} src='/img/logo.png' />
-					<div className='headerInfo'>
-						<h3>React Sneakers</h3>
-						<p>Магазин лучших кроссовок</p>
-					</div>
-				</div>
-				<ul className='headerRight'>
-					<li>
-						<img width={18} height={18} src='/img/cart.svg' />
-						<span>1205 руб.</span>
-					</li>
-					<li>
-						<img width={18} height={18} src='/img/user.svg' />
-					</li>
-				</ul>
-			</header>
-			<div className='content'>
+		<div className='wrapper clear'>
+			<Header />
+			<div className='content p-40'>
 				<h1>Все кроссовки</h1>
 			</div>
 		</div>
